83f61d5089ca4638d70443bcf61246330cbd440e
Sam Peacey <sam.peacey@gmail.com>
2018-08-05 10:02:08 +0900

    fixed labels, create folder for stack on deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,7 +2,7 @@
 
 (
   ssh $SSH_USERNAME@$SSH_HOSTNAME -o StrictHostKeyChecking=no <<-EOF
-    cd /home/cheeseweasel/apps
+    cd /home/cheeseweasel/apps/mindfulnessreminder
     docker login --password $DOCKER_PASSWORD --username $DOCKER_USER
     docker stack deploy -c mindfulnessreminder-stack.yml mindfulnessreminder
 EOF
